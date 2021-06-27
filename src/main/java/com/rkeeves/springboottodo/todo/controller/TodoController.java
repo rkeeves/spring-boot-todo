a68529565130db5f7fa5394c8d94a40ce1ba0395
rkeeves <aprettydecentaddress@gmail.com>
2021-06-27 22:54:58 +0200

    feat: Todo Controller supports delete

--- src/main/java/com/rkeeves/springboottodo/todo/controller/TodoController.java
+++ src/main/java/com/rkeeves/springboottodo/todo/controller/TodoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -35,6 +36,12 @@ public class TodoController {
             return "todo/add";
         }
         todoService.createAndSaveNewTodo(todoCreateDTO);
+        return "redirect:/";
+    }
+
+    @GetMapping("/todo/delete/{todoId}")
+    public String showFormCreateTodo(@PathVariable Long todoId){
+        todoService.delete(todoId);
         return "redirect:/";
     }
 }
