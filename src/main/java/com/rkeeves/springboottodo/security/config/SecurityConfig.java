fd2afceb9b16dba37b2799b12cc553453adc5455
rkeeves <aprettydecentaddress@gmail.com>
2021-06-28 16:33:03 +0200

    feat: block users from deletion of todos

--- src/main/java/com/rkeeves/springboottodo/security/config/SecurityConfig.java
+++ src/main/java/com/rkeeves/springboottodo/security/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/todo/delete/**").hasRole("ADMIN")
                 .antMatchers("/").hasAnyRole("ADMIN", "USER")
                 .and()
                 .formLogin(loginConfig->{
